664498e57a8f6a0798cc31d8c88546e988fa8c43
machao <2653406590@qq.com>
2018-12-18 14:30:51 +0800

    4.6-logout

--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ import router from './router'
 import './directives'
 import './components'
 import store from './store'
-
-
+// 使用插件
 import axios from 'axios'
 import Qs from 'qs'
 //QS是axios库中带的，不需要我们再npm安装一个
+// 引入插件
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+
+Vue.use(VueSweetalert2)
  
 Vue.prototype.axios = axios;
 Vue.prototype.qs = Qs;
